6071bb748db90653f3424ecb9f394b5bf69c5ea7
Gabriel Andersson <andersson.b.gabriel@gmail.com>
2022-10-05 17:24:46 +0200

    Completed Module

--- ConferenceTracker/Controllers/SpeakersController.cs
+++ ConferenceTracker/Controllers/SpeakersController.cs
@@ -46,10 +46,19 @@ namespace ConferenceTracker.Controllers
 
         [HttpPost]
         [Authorize(Roles = "Administrators")]
-        public IActionResult Create(Speaker speaker)
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id, FirstName, LastName, Description, EmailAddress, PhoneNumber")]
+        Speaker speaker)
         {
-            _speakerRepository.Create(speaker);
-            return RedirectToAction(nameof(Index));
+            if (ModelState.IsValid)
+            {
+                _speakerRepository.Create(speaker);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(speaker);
+            }
         }
 
         [HttpGet]
